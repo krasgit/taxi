d73c9516fd2e469d001b97a76b7035983d2fd351
kivanov <ki@unicard-uk.com>
2024-08-09 08:53:09 +0300

    add map serv

---
+++ maps/site/Leaflet Routing Machine_files/config.js
@@ -0,0 +1,21 @@
+/*
+ * Hi there,
+ * Please, PLEASE do not re-use this token in your own projects or anywhere else.
+ * This is a free tier token on openrouteservice.org that has limited quota as is. It is solely meant as a demo token for
+ * Leaflet Routing Machine, not to fund other projects.
+ *
+ * Please get your own OpenRouteService account if you like their services, it's not that expensive.
+ * I and the other contributors to Leaflet Routing Machine give it and its source away for free,
+ * stealing this key from me to save a few bucks feels a bit cheap, to be honest.
+ */
+window.LRM = {
+//	tileLayerUrl: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+    tileLayerUrl: 'http://localhost:8080/tile/{z}/{x}/{y}.png',
+
+	osmServiceUrl: 'https://routing.openstreetmap.de/routed-car/route/v1',
+    //https://nominatim.openstreetmap.org/search?q=varna&limit=5&format=json&addressdetails=1
+	orsServiceUrl: 'https://api.openrouteservice.org/geocode/',
+	apiToken: '5b3ce3597851110001cf6248ff41dc332def43858dff1ecccdd19bbc'
+};
+
+console.log("                    ___\n                _.-'   ```'--.._                 _____ ___ ___   ____  _____ __ __      ______  __ __    ___  \n              .'                `-._            / ___/|   |   | /    |/ ___/|  |  |    |      ||  |  |  /  _] \n             /                      `.         (   \\_ | _   _ ||  o  (   \\_ |  |  |    |      ||  |  | /  [_        \n            /                         `.        \\__  ||  \\_/  ||     |\\__  ||  _  |    |_|  |_||  _  ||    _]       \n           /                            `.      /  \\ ||   |   ||  _  |/  \\ ||  |  |      |  |  |  |  ||   [_        \n          :       (                       \\     \\    ||   |   ||  |  |\\    ||  |  |      |  |  |  |  ||     |       \n          |    (   \\_                  )   `.    \\___||___|___||__|__| \\___||__|__|      |__|  |__|__||_____|       \n          |     \\__/ '.               /  )  ;  \n          |   (___:    \\            _/__/   ;    ____   ____  ______  ____   ____   ____  ____      __  __ __  __ __ \n          :       | _  ;          .'   |__) :   |    \\ /    ||      ||    \\ |    | /    ||    \\    /  ]|  |  ||  |  |\n           :      |` \\ |         /     /   /    |  o  )  o  ||      ||  D  ) |  | |  o  ||  D  )  /  / |  |  ||  |  |\n            \\     |_  ;|        /`\\   /   /     |   _/|     ||_|  |_||    /  |  | |     ||    /  /  /  |  _  ||  ~  |\n             \\    ; ) :|       ;_  ; /   /      |  |  |  _  |  |  |  |    \\  |  | |  _  ||    \\ /   \\_ |  |  ||___, |\n              \\_  .-''-.       | ) :/   /       |  |  |  |  |  |  |  |  .  \\ |  | |  |  ||  .  \\\\     ||  |  ||     |\n             .-         `      .--.'   /        |__|  |__|__|  |__|  |__|\\_||____||__|__||__|\\_| \\____||__|__||____/ \n            :         _.----._     `  < \n            :       -'........'-       `.\n             `.        `''''`           ;\n               `'-.__                  ,'\n                     ``--.   :'-------'\n                         :   :\n                        .'   '.\n      \n      \n                                                                    ");
