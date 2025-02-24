7d2d8cd481f476faf4d8a9d066eeeddda1edd220
kivanov <kivanov@kivanov>
2025-02-24 20:07:03 +0200

    use template strings

--- src/main/resources/static/resources/openlayers/mylib.js
+++ src/main/resources/static/resources/openlayers/mylib.js
@@ -3,6 +3,16 @@
 export const sampleFunction=()=>{
 alert("Hello I'm sample");
 
+let firstName='kras';
+let lastName='ivan';
+let txt=
+`1
+
+My Name ${firstName} ${lastName}
+
+2`;
+
+return txt;
 }
 
 
