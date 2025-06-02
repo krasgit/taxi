501f963669791a7ca4f3ea6d453c368aac4a18bf
kivanov <kivanov@kivanov>
2025-06-02 22:14:07 +0300

    add proffer table

---
+++ src/main/java/com/matin/taxi/db/model/ProfferMapper.java
@@ -0,0 +1,20 @@
+package com.matin.taxi.db.model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+public class ProfferMapper implements RowMapper<Proffer> {
+
+	public Proffer mapRow(ResultSet rs, int i) throws SQLException {
+
+		Proffer proffer = new Proffer();
+		proffer.setId(rs.getInt("id"));
+		proffer.setState(rs.getInt("state"));
+		proffer.setOrderId(rs.getInt("orderId"));
+		
+		proffer.setCreated_at(rs.getTimestamp("created_at"));
+		return proffer;
+	}
+}
